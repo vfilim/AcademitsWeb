7952551e0ba48e1f2ffa9344f321f007403ad38b
vfilim <rashovlad@gmail.com>
2021-07-20 22:23:41 +0700

    Первая итерация задания "Underscore"

---
+++ Underscore/js/script.js
@@ -0,0 +1,83 @@
+(function () {
+    var people = [
+        {
+            age: 22,
+            name: "Alice",
+            lastName: "Smith"
+        },
+        {
+            age: 42,
+            name: "John",
+            lastName: "Jackson"
+        },
+        {
+            age: 36,
+            name: "Mary",
+            lastName: "Trump"
+        },
+        {
+            age: 33,
+            name: "Jack",
+            lastName: "Shephard"
+        },
+        {
+            age: 29,
+            name: "Ben",
+            lastName: "Shapiro"
+        },
+        {
+            age: 17,
+            name: "Wolfgang",
+            lastName: "Goethe"
+        },
+        {
+            age: 25,
+            name: "Eliza",
+            lastName: "Bach"
+        },
+        {
+            age: 67,
+            name: "John",
+            lastName: "Tekkery"
+        },
+        {
+            age: 82,
+            name: "Isac",
+            lastName: "Shapiro"
+        },
+        {
+            age: 74,
+            name: "Kira",
+            lastName: "King"
+        },
+    ];
+
+    var averageAge = _.chain(people)
+        .pluck('age')
+        .reduce(function (memo, num) {
+            return memo + num
+        }, 0)
+        .value() / people.length;
+
+    console.log(averageAge);
+
+    var fromAge = 20;
+    var toAge = 30;
+
+    var chosenAgePeople = _.chain(people)
+        .filter(function (human) {
+            return human.age >= fromAge && human.age <= toAge;
+        })
+        .sortBy("age")
+        .value()
+
+    console.log(chosenAgePeople);
+
+    var peopleWithFullNames = _.each(people, function (e) {
+        e.FullName = e.lastName + " " + e.name;
+
+        return;
+    });
+
+    console.log(peopleWithFullNames);
+}())
