9ade589278078051ed8ef97bba2466b812c18bb2
vfilim <rashovlad@gmail.com>
2021-07-24 15:12:03 +0700

    Третья итерация задачи "ToDo List jQuery"

--- TodoListJQuery/js/script.js
+++ TodoListJQuery/js/script.js
@@ -11,23 +11,42 @@ $(function () {
             .addClass("entry");
 
         var editEntryButton = $("<button>Edit</button>")
+            .addClass("todo-list-button")
             .click(function () {
                 newEntry.children().detach();
 
-                var editEntryInput = $("<input></input>").val(newEntry.text());
+                var editEntryInput = $("<input>").val(newEntry.text());
 
-                newEntry.html("")
+                var oldText = newEntry.text();
+
+                newEntry.html("");
                 editEntryInput.appendTo(newEntry);
 
                 $("<button>Save</button>").appendTo(newEntry)
                     .addClass("todo-list-button")
                     .click(function () {
+                        if (editEntryInput.val() === "") {
+                            alert("The edit can't be empty");
+
+                            return;
+                        }
+
                         newEntry.append(newEntry.children(1).val());
                         newEntry.children().detach();
 
                         newEntry.append(editEntryButton);
                         newEntry.append(deleteButton);
-                    })
+                    });
+
+                $("<button>Cancel</button>").appendTo(newEntry)
+                    .addClass("todo-list-button")
+                    .click(function () {
+                        newEntry.text(oldText);
+                        newEntry.children().detach();
+
+                        newEntry.append(editEntryButton);
+                        newEntry.append(deleteButton);
+                    });
             })
             .addClass("todo-list-button")
             .appendTo(newEntry);
