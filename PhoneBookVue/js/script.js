a5d9082e4e79fd78997a27322b5843fab8851973
vfilim <rashovlad@gmail.com>
2021-08-04 22:36:27 +0700

    Первая итерация задания PhoneBook Vue

---
+++ PhoneBookVue/js/script.js
@@ -0,0 +1,73 @@
+var vm = new Vue({
+    el: "#app",
+
+    data: {
+        newId: 1,
+        items: [],
+
+        firstName: "",
+        lastName: "",
+        phoneNumber: "",
+
+        isFirstNameCorrect: true,
+        isLastNameCorrect: true,
+        isPhoneNumberCorrect: true
+    },
+
+    methods: {
+        addEntry: function () {
+            this.isFirstNameCorrect = true;
+            this.isLastNameCorrect = true;
+            this.isPhoneNumberCorrect = true;
+
+            var isEntryCorrect = true;
+
+            if (this.firstName.trim() === "") {
+                this.isFirstNameCorrect = false;
+                isEntryCorrect = false;
+            }
+
+            if (this.lastName.trim() === "") {
+                this.isLastNameCorrect = false;
+                isEntryCorrect = false;
+            }
+
+            if (this.phoneNumber.trim() === "") {
+                this.isPhoneNumberCorrect = false;
+                isEntryCorrect = false;
+            }
+
+            if (!isEntryCorrect) {
+                return;
+            }
+
+            this.items.push({
+                id: this.newId,
+                firstName: this.firstName,
+                lastName: this.lastName,
+                phoneNumber: this.phoneNumber
+            });
+
+            this.newId++;
+            this.firstName = "";
+            this.lastName = "";
+            this.phoneNumber = "";
+        },
+
+        deleteEntry: function (item) {
+            this.items = this.items.filter(function (x) {
+                return x.id !== item.id;
+            });
+
+            this.recalculateIds();
+        },
+
+        recalculateIds: function () {
+            this.newId = this.items.length + 1;
+
+            this.items.forEach(function (object, index) {
+                object.id = index + 1;
+            });
+        }
+    }
+});
